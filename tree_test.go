fd98e50378af0fba86c3208710b5d3878cdceebc
lovemu <weibaohui@yeah.net>
2016-08-07 13:57:35 +0800

    tree_test

---
+++ tree_test.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+type People struct {
+	Name string
+	Age  int
+	sex  string
+
+	Father   *People
+	Children []*People
+	Pro      map[string]string
+}
+
+func Test_print(t *testing.T) {
+	fa := &People{Name: "jim", Age: 44, sex: "man"}
+	fa.Pro = make(map[string]string)
+	fa.Pro["city"] = "Beijing"
+	son := &People{Name: "jimson", Age: 12, sex: "boy"}
+	daughter := &People{Name: "jimdaughter", Age: 14, sex: "girl"}
+	fa.Children = make([]*People, 2)
+	fa.Children[0] = son
+	fa.Children[1] = daughter
+	son.Father = fa
+	//Print(fa)
+	fmt.Println(Sprint(fa))
+}
