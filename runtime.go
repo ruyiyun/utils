1635fdd153a739077e1e82c2f0f8180c5d6f4b12
lovemu <weibaohui@yeah.net>
2016-08-08 16:41:21 +0800

    增加运行时的获取方法

--- runtime.go
+++ runtime.go
@@ -6,9 +6,11 @@ import (
 )
 
 //返回调用此方法的方法名及key值,组合为缓存用的Key
-func GetExecFuncId(key interface{}) (Id string) {
+func GetExecFuncId(key ...interface{}) (Id string) {
+
 	if funcName, _, _, ok := runtime.Caller(1); ok {
 		Id = fmt.Sprintf("%v_%v", runtime.FuncForPC(funcName).Name(), key)
 	}
+
 	return
 }
